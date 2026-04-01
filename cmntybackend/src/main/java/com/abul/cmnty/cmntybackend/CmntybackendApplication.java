2cf9f5ebd598e1343b157c24e9598c04a6134b1c
Abul <hassanabul9279@gmail.com>
2026-04-01 23:12:31 +0530

    cmnty-first push

---
+++ cmntybackend/src/main/java/com/abul/cmnty/cmntybackend/CmntybackendApplication.java
@@ -0,0 +1,13 @@
+package com.abul.cmnty.cmntybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmntybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmntybackendApplication.class, args);
+	}
+
+}
---
+++ cmntybackend/src/test/java/com/abul/cmnty/cmntybackend/CmntybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abul.cmnty.cmntybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmntybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
